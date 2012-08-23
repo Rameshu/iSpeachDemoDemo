7fbea119790b42ee2ca93347f99c4965471637e8
Ramesh Chand <iphone@NeuronMac2s-Mac-mini.local>
2012-08-23 15:13:23 +0530

    Initial Commit

---
+++ iSpeachDemo/FBCDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FBCDAppDelegate.h
+//  iSpeachDemo
+//
+//  Created by iPhone on 23/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FBCDViewController;
+
+@interface FBCDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FBCDViewController *viewController;
+
+@end
---
+++ iSpeachDemo/FBCDViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBCDViewController.h
+//  iSpeachDemo
+//
+//  Created by iPhone on 23/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDViewController : UIViewController
+
+@end
